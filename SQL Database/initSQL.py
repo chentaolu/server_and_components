f3940a2396c738bf88e8767c489960945c458d0e
chentaolu <chentaolu@mail.fcu.edu,tw>
2021-06-28 21:12:42 +0800

    add a name in sql

--- SQL Database/initSQL.py
+++ SQL Database/initSQL.py
@@ -14,8 +14,9 @@ sql = 'DROP TABLE IF EXISTS `Game_Data`'
 cur.execute(sql)
 
 sql = 'CREATE TABLE `Game_Data` (' + \
-    '`id` int(11),' + \
-    '`score` int(11),' +\
+    '`id` int(11) NOT NULL AUTO_INCREMENT,' + \
+    '`score` int(11) DEFAULT NULL,' + \
+    '`name` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,' + \
     ' PRIMARY KEY (`id`)' + \
     ') ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci'
 cur.execute(sql)
