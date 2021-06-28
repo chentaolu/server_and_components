25c5291987c40db406637178614ce3b0c976db5a
chentaolu <chentaolu@mail.fcu.edu,tw>
2021-06-28 20:59:49 +0800

    add init sql.py

---
+++ SQL Database/initSQL.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 28 16:49:06 2021
+
+@author: owner
+"""
+import  pymysql 
+
+conn = pymysql.connect(
+    host = '127.0.0.1', user = 'root', passwd = 'root', db = 'sys'
+    )
+cur  =  conn.cursor () 
+sql = 'DROP TABLE IF EXISTS `Game_Data`'
+cur.execute(sql)
+
+sql = 'CREATE TABLE `Game_Data` (' + \
+    '`id` int(11),' + \
+    '`score` int(11),' +\
+    ' PRIMARY KEY (`id`)' + \
+    ') ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci'
+cur.execute(sql)
