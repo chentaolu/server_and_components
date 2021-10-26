453ed0d42cdba1e9bbea8d16ed477ce668e77a30
chentaolu <chentaolu@mail.fcu.edu,tw>
2021-10-26 21:41:40 +0800

    新增計算

--- pythonClient/testClient.py
+++ pythonClient/testClient.py
@@ -8,6 +8,11 @@ Created on Fri May 14 17:43:45 2021
 import socket
 import threading
 import json
+import numpy as np
+from scipy.spatial.transform import Rotation as R
+from numba import jit
+from numpy.linalg import inv
+
 
 host = '127.0.0.1'
 port = 5678
@@ -26,6 +31,11 @@ class Queue:
             return False
         else:
             return True
+        
+@jit(nopython=True)
+def calculateLength(x, y, z):
+    return ((x * x) + (y * y) + (z * z)) ** (1 / 2)
+
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((host, port))
@@ -43,25 +53,44 @@ def job(socket):
         if(not dataQueue.IsEmpty()):
             result = dict()
             currentData = dataQueue.Pop()
+            if(currentData['speedChange'] == 'centerArduino') :
             
-            if (originSpeed == -1) :
-                fanSpeed = int(currentData['CurrentSpeed']) // 10
-                
-                result.setdefault('sendTo', currentData['speedChange'])
-                result.setdefault('fanSpeed', fanSpeed)
-                
-                result = str(result).replace("\'", "\"") + "\n"
-                socket.send(bytes(result, encoding = "utf8"))
-                originSpeed = fanSpeed
-            else :
-                fanSpeed = int(currentData['CurrentSpeed']) // 10
-                if (originSpeed != fanSpeed) :
+                if (originSpeed == -1) :
+                    fanSpeed = int(currentData['CurrentSpeed']) // 10
+                    
                     result.setdefault('sendTo', currentData['speedChange'])
                     result.setdefault('fanSpeed', fanSpeed)
+                    
                     result = str(result).replace("\'", "\"") + "\n"
                     socket.send(bytes(result, encoding = "utf8"))
                     originSpeed = fanSpeed
-
+                    
+                else :
+                    fanSpeed = int(currentData['CurrentSpeed']) // 10
+                    if (originSpeed != fanSpeed) :
+                        result.setdefault('sendTo', currentData['speedChange'])
+                        result.setdefault('fanSpeed', fanSpeed)
+                        result = str(result).replace("\'", "\"") + "\n"
+                        socket.send(bytes(result, encoding = "utf8"))
+                        originSpeed = fanSpeed
+                        
+            elif(currentData['speedChange'] == 'otherArduino') :
+                r = R.from_quat([currentData['Quaternions']['x'], currentData['Quaternions']['y'], currentData['Quaternions']['z'], currentData['Quaternions']['w']])
+                rotationMatrix = inv(r.as_matrix())
+                newXAxis = rotationMatrix.dot(np.array([1, 0, 0]).T)
+                newYAxis = rotationMatrix.dot(np.array([0, 1, 0]).T)
+                newZAxis = rotationMatrix.dot(np.array([0, 0, 1]).T)
+                BMatrix = np.column_stack([list(newXAxis), list(newYAxis), list(newZAxis)]).T
+                currentFanLocation = np.array([currentData['fanLocation']['x'], currentData['fanLocation']['y'], currentData['fanLocation']['z']]).T
+                newFanLocation = np.linalg.inv(BMatrix).dot(currentFanLocation)
+                
+                fanSpeed = (calculateLength(currentData['fanLocation']['x'] - currentData['currentLocation']['x'], currentData['fanLocation']['y'] - currentData['currentLocation']['y'], currentData['fanLocation']['z'] - currentData['currentLocation']['z']) // 200) * 100
+                
+                result.setdefault('fanSpeed', fanSpeed)
+                result = str(result).replace("\'", "\"") + "\n"
+                socket.send(bytes(result, encoding = "utf8"))
+                
+                
 sendThread = threading.Thread(target = job , args = (s,))
 sendThread.start()
 
